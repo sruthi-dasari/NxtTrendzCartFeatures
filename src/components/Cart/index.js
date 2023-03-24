955bd776c7a9fc5dc59292ad9b78d2e09f0adfe2
sruthi <sruthi@example.com>
2023-03-24 10:42:38 +0000

    in progress

---
+++ src/components/Cart/index.js
@@ -0,0 +1,59 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      const cartCount = cartList.length
+
+      // TODO: Update the functionality to remove all the items in the cart
+
+      const onClickRemoveAll = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  type="button"
+                  className="remove-all-btn"
+                  onClick={onClickRemoveAll}
+                  data-testid="remove"
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <div className="cart-summary-container">
+                  <h1 className="cart-summary-heading">
+                    Order Total:
+                    <span className="cart-summary-span"> Rs {}/-</span>
+                  </h1>
+                  <p className="cart-summary-para">{cartCount} Items in cart</p>
+                </div>
+                <button type="button" className="checkout-btn">
+                  Checkout
+                </button>
+                {/* TODO: Add your code for Cart Summary here */}
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
