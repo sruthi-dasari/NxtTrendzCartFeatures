8f37f7cc955ff2730bd757aa7ca9a522037318cd
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-28 19:43:31 +0530

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,1 +1,20 @@
-// Write your code here
+import './index.css'
+
+const CartSummary = props => {
+  const {cartCount, totalAmt} = props
+
+  return (
+    <div className="cart-summary-container">
+      <h1 className="cart-summary-heading">
+        Order Total:
+        <span className="cart-summary-span"> Rs {totalAmt}/-</span>
+      </h1>
+      <p className="cart-summary-para">{cartCount} Items in cart</p>
+
+      <button type="button" className="checkout-btn">
+        Checkout
+      </button>
+    </div>
+  )
+}
+export default CartSummary
